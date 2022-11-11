dcc2a355fb22efbdc4f993eadc6b9d32f607b01b
Clare <clarecat.cd@gmail.com>
2022-11-10 17:40:24 -0700

    Song filtering and chart data

--- assign2.js
+++ assign2.js
@@ -1,9 +1,86 @@
 document.addEventListener("DOMContentLoaded", ()=>{
    let artists = JSON.parse(Artists);
    let genres = JSON.parse(Genres);
+
+   //replace this with api fetch *******************************************************************
+   //**************************************************************************************************
+   //**************************************************************************************************
+   //**************************************************************************************************
+   let songs = JSON.parse(Songs);
+
+   //populate table
+   let songTable = document.querySelector("#browseView table");
+   populateSongs(songs, songTable);
+
+   function populateSongs(songs, table){
+      let counter = 0;
+      table.innerHTML = "";
+
+      let labelRow = document.createElement("tr");
+      labelRow.className = "labels";
+      let d = document.createElement("th");
+      d.innerText = "Title";
+      labelRow.appendChild(d);
+      let d1 = document.createElement("th");
+      d1.innerText = "Artist";
+      labelRow.appendChild(d1);
+      let d2 = document.createElement("th");
+      d2.innerText = "Year";
+      labelRow.appendChild(d2);
+      let d3 = document.createElement("th");
+      d3.innerText = "Genre";
+      labelRow.appendChild(d3);
+      let d4 = document.createElement("th");
+      d4.innerText = "Popularity";
+      labelRow.appendChild(d4);
+      let d5 = document.createElement("td");
+      labelRow.appendChild(d5);
+      
+      table.appendChild(labelRow);
+
+
+      for(song of songs){
+         let row = document.createElement("tr");
+         row.className = "stripe";
+         if(counter%2 != 0){
+            row.classList.add("darkStripe");
+         }
    
-   let artistSelector = document.querySelector("select.Artist");
-   let genreSelector = document.querySelector("select.Genre");
+         let td = document.createElement("td");
+         td.innerText = song.title;
+         td.dataset['song_id'] = song.song_id;
+         row.appendChild(td);
+         let td1 = document.createElement("td");
+         td1.innerText = song.artist.name;
+         td1.dataset['song_id'] = song.song_id;
+         row.appendChild(td1);
+         let td2 = document.createElement("td");
+         td2.innerText = song.year;
+         td2.dataset['song_id'] = song.song_id;
+         row.appendChild(td2);
+         let td3 = document.createElement("td");
+         td3.innerText = song.genre.name;
+         td3.dataset['song_id'] = song.song_id;
+         row.appendChild(td3);
+         let td4 = document.createElement("td");
+         td4.innerText = song.details.popularity;
+         td4.dataset['song_id'] = song.song_id;
+         row.appendChild(td4);
+         let td5 = document.createElement("td");
+         let button = document.createElement("button");
+         button.dataset['song_id'] = song.song_id;
+         button.innerText = "+";
+         td5.appendChild(button);
+         row.appendChild(td5);
+   
+         table.appendChild(row);
+         counter++;
+      }
+   }
+
+   
+   let artistSelector = document.querySelector("select.artist");
+   let genreSelector = document.querySelector("select.genre");
 
    for(artist of artists){
       let option = document.createElement("option");
@@ -48,6 +125,49 @@ document.addEventListener("DOMContentLoaded", ()=>{
       }
    });
 
+   //filter search results
+   let searchButton = document.querySelector('#search');
+   searchButton.addEventListener("click", function(){
+      let matchedSongs = songs;
+      for(button of radioButtons){
+         if(button.classList.contains('selected')){
+            let searchTerm = button.classList[1];
+            if(searchTerm == "year"){
+               let range = document.querySelectorAll("#year input");
+               if(range[0].value == "")
+                  range[0].value = 2022;
+               if(range[1].value == "")
+                  range[1].value = 0; 
+               matchedSongs = matchedSongs.filter(d => d.year >= range[1].value && d.year <= range[0].value);
+            }
+            else if(searchTerm == "popularity"){
+               let range = document.querySelectorAll("#popularity input");
+               if(range[0].value == "")
+                  range[0].value = 100;
+               if(range[1].value == "")
+                  range[1].value = 0; 
+               matchedSongs = matchedSongs.filter(d => d.details.popularity >= range[1].value && d.details.popularity <= range[0].value);
+            }
+            else{
+               if(searchTerm == "title"){
+                  let searchValue = document.querySelector(`input.${searchTerm}`).value;
+                  matchedSongs = matchedSongs.filter(d => d.title == searchValue);
+               }
+               else if(searchTerm == "genre"){
+                  let searchValue = document.querySelector(`select.${searchTerm}`).value;
+                  matchedSongs = matchedSongs.filter(d => d.genre.name == searchValue);
+               }
+               else if(searchTerm == "artist"){
+                  let searchValue = document.querySelector(`select.${searchTerm}`).value;
+                  matchedSongs = matchedSongs.filter(d => d.artist.name == searchValue);
+               }
+            }
+         }
+      }
+
+      populateSongs(matchedSongs, songTable);
+   });
+
 
    //display credits for 5 seconds when hovered over
    let credits = document.querySelector("#credits");
@@ -59,10 +179,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
       }, 5000);
    });
 
+   //switch viewports
+   let closeButton = document.querySelector("#close");
+   let playlistButton = document.querySelector("#playlist");
+   
+   let browseView = document.querySelector("#browseView");
+   let singleView = document.querySelector("#singleView");
+   let playlistView = document.querySelector("#playlistView");
+
+   playlistButton.addEventListener("click", function(){
+      browseView.classList.add("hide");
+      playlistView.classList.remove("hide");
+      playlistButton.classList.add("hide");
+      closeButton.classList.remove("hide");
+   });
+
+   closeButton.addEventListener("click", function(){
+      playlistView.classList.add("hide");
+      singleView.classList.add("hide");
+      browseView.classList.remove("hide");
+      closeButton.classList.add("hide");
+      playlistButton.classList.remove("hide");
+   });
 
    //radar chart
    const ctx = document.getElementById('radar').getContext('2d');
-   const data = {
+   const chartData = {
       labels: [
          'Danceability',
          'Liveness',
@@ -81,7 +223,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
    }
    const myChart = new Chart(ctx, {
       type: 'radar',
-      data: data,
+      data: chartData,
       options: {
          plugins: {
             dataLabels: {
@@ -98,9 +240,38 @@ document.addEventListener("DOMContentLoaded", ()=>{
                   display: false
                },
                 suggestedMin: 0,
-                suggestedMax: 100
             }
         }
+      }
+   });
+
+   const playlist = [];
+
+   songTable.addEventListener("click", function(e){
+      //add song to playlist
+      if(e.target && e.target.nodeName == "BUTTON"){
+         let song = songs.find(d => d.song_id == e.target.dataset.song_id);
+         if(playlist.find(d => d == song) == undefined){
+            playlist.push(song);
+            console.log(playlist);
+         }
+      }
+
+      //display song info
+      else if(e.target && e.target.nodeName == "TD"){
+         browseView.classList.add("hide");
+         singleView.classList.remove("hide");
+         playlistButton.classList.add("hide");
+         closeButton.classList.remove("hide");
+
+         let song = songs.find(d => d.song_id == e.target.dataset.song_id);
+
+         myChart.data.datasets[0].data[0] = song.analytics.danceability;
+         myChart.data.datasets[0].data[1] = song.analytics.liveness;
+         myChart.data.datasets[0].data[2] = song.analytics.valence;
+         myChart.data.datasets[0].data[3] = song.analytics.acousticness;
+         myChart.data.datasets[0].data[4] = song.analytics.speechiness;
+         myChart.update();
       }
    });
 })
